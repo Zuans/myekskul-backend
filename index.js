696b5f50e4b85ec32c8cdef64a5bf6bde952e3dc
Zuans <juandev.net@gmail.com>
2025-05-31 17:33:58 +0700

    config atlas

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const guruRoutes = require("./routes/guruRoutes");
 const siswaRoutes = require("./routes/siswaRoutes");
 const ekstrakurikulerRoutes = require("./routes/ekstrakurikulerRoutes");
 const absensiRoutes = require("./routes/absensiRoutes");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect("mongodb://localhost:27017/absensi-ekskul")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Atlas connected!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/guru", guruRoutes);
 app.use("/api/ekstrakurikuler", ekstrakurikulerRoutes);
